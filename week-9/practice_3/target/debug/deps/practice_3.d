0e7477c1e50c4ef81a7356286b6e378b9a866022
Absxonder <anthony.okocha-ojeah@pau.edu.ng>
2025-11-26 13:16:13 +0100

    Learnt how to make rust to read a file

---
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\HomePC\Documents\ALL MY NOTES WITH COURSES\FIRST YEAR\FIRST SEMESTER\COS 101\a.okocha-ojeahCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\HomePC\Documents\ALL MY NOTES WITH COURSES\FIRST YEAR\FIRST SEMESTER\COS 101\a.okocha-ojeahCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
---
+++ week-9/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\ALL\ MY\ NOTES\ WITH\ COURSES\FIRST\ YEAR\FIRST\ SEMESTER\COS\ 101\a.okocha-ojeahCOS101\week-9\practice_3\target\debug\practice_3.exe: C:\Users\HomePC\Documents\ALL\ MY\ NOTES\ WITH\ COURSES\FIRST\ YEAR\FIRST\ SEMESTER\COS\ 101\a.okocha-ojeahCOS101\week-9\practice_3\src\main.rs
