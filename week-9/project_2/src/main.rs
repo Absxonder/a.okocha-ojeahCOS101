c7f5de56e2bd2c88efce53fdb4ca00a9b82f4f86
Absxonder <anthony.okocha-ojeah@pau.edu.ng>
2025-11-26 21:40:15 +0100

    Read and printed an excel file named PAU SMIS

--- week-9/project_2/src/main.rs
+++ week-9/project_2/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 
 fn main(){
-    let mut file = std::fs::File::open("../../../PAU_SMIS_PROJECT_2_SAMPLE.csv").unwrap();
+    let mut file = std::fs::File::open("PAU_SMIS_PROJECT_2_SAMPLE.csv").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     print!("{}", contents);
