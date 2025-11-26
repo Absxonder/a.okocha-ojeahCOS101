7d2743e38f5707d13dd5838d11c3e4944794f232
Absxonder <anthony.okocha-ojeah@pau.edu.ng>
2025-11-27 00:10:46 +0100

    Merged 3 datasets to form one dataset using vectors

--- week-9/project_3/src/main.rs
+++ week-9/project_3/src/main.rs
@@ -1,3 +1,58 @@
 fn main() {
-    println!("Hello, world!");
+
+    let commisioners = vec![
+
+    "Aigbogun Alamba Daudun",
+
+    "Murtala Afeez Bendu",
+
+    "Okorocha Calistus Ogbona",
+
+    "Adewale Jimoh Akanbi",
+
+    "Osazuwa Faith Etieye",
+    ];
+
+
+    let ministries = vec![
+
+    "Internal Affairs",
+
+    "Justice",
+
+    "Defense",
+
+    "Power & Steel",
+
+    "Petroleum",
+    ];
+
+
+    let geopolitical_zone = vec![
+
+    "South West",
+
+    "North East",
+
+    "South South",
+
+    "South West",
+
+    "South East",
+
+    ];
+
+
+   println!("__________________________________________________________________________________________________");
+
+    println!("| S/N |COMMISSIONER        | MINISTRY  | GEOPOLITICAL ZONE |");
+
+    println!("__________________________________________________________________________________________________");
+
+    for i in 1..commisioners.len() {  
+        
+        println!("| {} | {} | {} | {} |",i,commisioners[i], ministries[i], geopolitical_zone[i]);
+    }
+
+    println!("__________________________________________________________________________________________________");
 }
